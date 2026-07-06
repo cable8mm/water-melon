4f95e62c07d49e86d03072eb6fa3e697474e8f3d
Samgu Lee <cable8mm@gmail.com>
2026-07-07 01:42:47 +0900

    test: add core testing

---
+++ tests/ExceptionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Cable8mm\WaterMelon\Tests;
+
+use Cable8mm\WaterMelon\Exceptions\MelonApiException;
+use Cable8mm\WaterMelon\MelonSong;
+use PHPUnit\Framework\TestCase;
+
+final class ExceptionTest extends TestCase
+{
+    public function test_song_exception_has_correct_message(): void
+    {
+        $this->expectException(MelonApiException::class);
+        $this->expectExceptionMessage("Invalid API response structure: missing 'response' key");
+
+        $song = new MelonSong(999999999); // Invalid song ID
+        $song->parse();
+    }
+
+    public function test_exception_factory_methods(): void
+    {
+        $exception1 = MelonApiException::invalidResponse('test_key');
+        $this->assertSame("Invalid API response structure: missing 'test_key' key", $exception1->getMessage());
+
+        $exception2 = MelonApiException::requestFailed('http://test.com', 'Connection timeout');
+        $this->assertSame('Failed to fetch data from http://test.com: Connection timeout', $exception2->getMessage());
+
+        $exception3 = MelonApiException::jsonParseError('http://test.com', 'Syntax error');
+        $this->assertSame('Failed to parse JSON response from http://test.com: Syntax error', $exception3->getMessage());
+
+        $exception4 = MelonApiException::emptyResponse('http://test.com');
+        $this->assertSame('Empty response received from http://test.com', $exception4->getMessage());
+    }
+
+    public function test_exception_is_instance_of_exception(): void
+    {
+        $exception = MelonApiException::invalidResponse('test');
+
+        $this->assertInstanceOf(\Exception::class, $exception);
+        $this->assertInstanceOf(\Throwable::class, $exception);
+    }
+}
---
+++ tests/InterfaceImplementationTest.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Cable8mm\WaterMelon\Tests;
+
+use Cable8mm\WaterMelon\Contracts\AlbumInterface;
+use Cable8mm\WaterMelon\Contracts\AlbumResourceInterface;
+use Cable8mm\WaterMelon\Contracts\ArtistInterface;
+use Cable8mm\WaterMelon\Contracts\ArtistResourceInterface;
+use Cable8mm\WaterMelon\Contracts\SongInterface;
+use Cable8mm\WaterMelon\Contracts\SongResourceInterface;
+use Cable8mm\WaterMelon\MelonAlbum;
+use Cable8mm\WaterMelon\MelonArtist;
+use Cable8mm\WaterMelon\MelonSong;
+use Cable8mm\WaterMelon\Resources\AlbumResource;
+use Cable8mm\WaterMelon\Resources\ArtistResource;
+use Cable8mm\WaterMelon\Resources\SongResource;
+use PHPUnit\Framework\TestCase;
+
+final class InterfaceImplementationTest extends TestCase
+{
+    public function test_melon_song_implements_song_interface(): void
+    {
+        $song = MelonSong::make(35945927);
+
+        $this->assertInstanceOf(SongInterface::class, $song);
+        $this->assertInstanceOf(\ArrayAccess::class, $song);
+    }
+
+    public function test_melon_album_implements_album_interface(): void
+    {
+        $album = MelonAlbum::make(11127145);
+
+        $this->assertInstanceOf(AlbumInterface::class, $album);
+        $this->assertInstanceOf(\ArrayAccess::class, $album);
+    }
+
+    public function test_melon_artist_implements_artist_interface(): void
+    {
+        $artist = MelonArtist::make(3114174);
+
+        $this->assertInstanceOf(ArtistInterface::class, $artist);
+        $this->assertInstanceOf(\ArrayAccess::class, $artist);
+    }
+
+    public function test_song_resource_implements_interface(): void
+    {
+        $song = MelonSong::make(35945927);
+        $resource = SongResource::make($song);
+
+        $this->assertInstanceOf(SongResourceInterface::class, $resource);
+    }
+
+    public function test_album_resource_implements_interface(): void
+    {
+        $album = MelonAlbum::make(11127145);
+        $resource = AlbumResource::make($album);
+
+        $this->assertInstanceOf(AlbumResourceInterface::class, $resource);
+    }
+
+    public function test_artist_resource_implements_interface(): void
+    {
+        $artist = MelonArtist::make(3114174);
+        $resource = ArtistResource::make($artist);
+
+        $this->assertInstanceOf(ArtistResourceInterface::class, $resource);
+    }
+
+    public function test_song_interface_has_required_methods(): void
+    {
+        $song = MelonSong::make(35945927);
+
+        // Test that all interface methods are callable
+        $this->assertIsCallable([$song, 'parse']);
+        $this->assertIsCallable([$song, 'getId']);
+        $this->assertIsCallable([$song, 'getTitle']);
+        $this->assertIsCallable([$song, 'getAlbumId']);
+        $this->assertIsCallable([$song, 'getArtworkImagePath']);
+    }
+
+    public function test_album_interface_has_required_methods(): void
+    {
+        $album = MelonAlbum::make(11127145);
+
+        $this->assertIsCallable([$album, 'parse']);
+        $this->assertIsCallable([$album, 'getId']);
+        $this->assertIsCallable([$album, 'getTitle']);
+        $this->assertIsCallable([$album, 'getAlbumCoverPath']);
+        $this->assertIsCallable([$album, 'getReleasedAt']);
+    }
+
+    public function test_artist_interface_has_required_methods(): void
+    {
+        $artist = MelonArtist::make(3114174);
+
+        $this->assertIsCallable([$artist, 'parse']);
+        $this->assertIsCallable([$artist, 'getId']);
+        $this->assertIsCallable([$artist, 'getName']);
+        $this->assertIsCallable([$artist, 'getFeaturedImagePath']);
+        $this->assertIsCallable([$artist, 'getProfileImagePath']);
+        $this->assertIsCallable([$artist, 'getBirth']);
+        $this->assertIsCallable([$artist, 'getDebut']);
+        $this->assertIsCallable([$artist, 'getAgency']);
+        $this->assertIsCallable([$artist, 'getGenre']);
+    }
+
+    public function test_song_resource_interface_has_required_methods(): void
+    {
+        $song = MelonSong::make(35945927);
+        $resource = SongResource::make($song);
+
+        $this->assertIsCallable([$resource, 'toArray']);
+        $this->assertIsCallable([$resource, 'getMelonSongId']);
+        $this->assertIsCallable([$resource, 'getTitle']);
+        $this->assertIsCallable([$resource, 'getAlbumId']);
+        $this->assertIsCallable([$resource, 'getArtworkImagePath']);
+    }
+
+    public function test_album_resource_interface_has_required_methods(): void
+    {
+        $album = MelonAlbum::make(11127145);
+        $resource = AlbumResource::make($album);
+
+        $this->assertIsCallable([$resource, 'toArray']);
+        $this->assertIsCallable([$resource, 'getMelonAlbumId']);
+        $this->assertIsCallable([$resource, 'getTitle']);
+        $this->assertIsCallable([$resource, 'getAlbumCoverPath']);
+        $this->assertIsCallable([$resource, 'getReleasedAt']);
+    }
+
+    public function test_artist_resource_interface_has_required_methods(): void
+    {
+        $artist = MelonArtist::make(3114174);
+        $resource = ArtistResource::make($artist);
+
+        $this->assertIsCallable([$resource, 'toArray']);
+        $this->assertIsCallable([$resource, 'getMelonArtistId']);
+        $this->assertIsCallable([$resource, 'getName']);
+        $this->assertIsCallable([$resource, 'getFeaturedImagePath']);
+        $this->assertIsCallable([$resource, 'getProfileImagePath']);
+        $this->assertIsCallable([$resource, 'getDebut']);
+    }
+}
